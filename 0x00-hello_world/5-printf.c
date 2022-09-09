301d88f5e5b8c65cdc9b0a7a86eb727c4b2c954f
Josephat <111060732+Aholi-Jay@users.noreply.github.com>
2022-09-09 00:14:56 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ *  * main -with proper grammar,
+ *   * but the outcome is a piece of art,
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *     */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
