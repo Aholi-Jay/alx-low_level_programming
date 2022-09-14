06dcbe9ea54bd7c2d66642e104e52d2d8e25b649
Josephat <111060732+Aholi-Jay@users.noreply.github.com>
2022-09-14 11:29:14 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,26 +8,22 @@
  * Return Always (0)
  *
  */
-
 int print_sign(int n)
-
 {
-	int i;
-
-	if (n > 0)
-	{
-		i = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		i = 0;
-		_putchar('0');
-	}
-	else
-	{
-		i = -1;
-		_putchar('-');
-	}
-	return (i);
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		}
+		else if (n == 0)
+		{
+			_putchar('0');
+			return (0);
+		}
+		else if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		return (0);
 }
