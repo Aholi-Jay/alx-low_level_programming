28585af793ed1946f5f2730f31e43fbfbfa1c5ec
Josephat A <111060732+Aholi-Jay@users.noreply.github.com>
2022-09-28 23:03:52 +0300

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * factorial - f factorial - function that returns the factorial of a given number 
+ * factorial - f factorial - function that returns factorial 
+ * of a given number
  * @n: int for arg
  *
  *Return: Always (0)
