5b754a5d2fd5449ebfabf4b78511e05c88f9ef88
Josephat A <111060732+Aholi-Jay@users.noreply.github.com>
2022-10-04 01:26:56 +0300

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,12 @@
 #inlude "main.h"
 
 /**
- * main - program that prints its name followed by a new line
+ * main - program that prints its name 
+ * followed by a new line
+ *
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: always (0)
  */
 
