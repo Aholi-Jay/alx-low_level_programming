1befa489ef19fc0f538c9f823d0110ec45013a13
Josephat <111060732+Aholi-Jay@users.noreply.github.com>
2022-11-02 05:42:04 +0300

    converts a binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,27 @@
 #include "main.h"
+
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: string containing binary num
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: points to a string of 0 and 1 characters
  *
- * Return: unsigned int
+ * Return: the converted number or 0
+ * string @b that is not 0 or 1, or @b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, i;
-	unsigned int sum = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
-		return (sum);
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (sum);
+	return (dec_val);
 }
